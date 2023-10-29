ec4bdee6550fe28275084c4cdea6800292789fd7
Pedro Marins <ph.marins@hotmail.com>
2023-10-29 00:04:31 -0300

    feat: implementing terraform

---
+++ main.tf
@@ -0,0 +1,18 @@
+provider "aws" {
+  region = var.aws_region
+}
+
+resource "aws_db_instance" "default" {
+  db_name = "teste"
+  allocated_storage = var.allocated_storage
+  storage_type = var.storage_type
+  engine = var.engine
+  port = var.port
+  engine_version = var.engine_version
+  instance_class = var.instance_class
+  username = var.username
+  password = var.password
+  identifier = var.identifier
+  skip_final_snapshot = var.skip_final_snapshot
+  publicly_accessible = var.publicly_accessible
+}
